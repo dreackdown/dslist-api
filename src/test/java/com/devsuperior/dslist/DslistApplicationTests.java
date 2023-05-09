92e6262d88675c976f6d0825fa347b4a550286b9
dreackdown <hugofaria157@live.com>
2023-05-08 23:10:22 -0300

    projeto criado

---
+++ src/test/java/com/devsuperior/dslist/DslistApplicationTests.java
@@ -0,0 +1,8 @@
+package com.devsuperior.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+}
